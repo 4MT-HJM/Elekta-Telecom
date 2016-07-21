6b7e8d4a166f687642a6bd88c8d133c3a945fc87
jiaminghuang <4MT-HJM@bitbucket.org>
2016-07-21 06:14:33 -0700

    add more UT case for Task-1

--- ElektaTelecom/Tests.cpp
+++ ElektaTelecom/Tests.cpp
@@ -11,3 +11,23 @@ TEST(tests, BillForaGoldAccountWithOneLineAndUsesLessThanOneThousandMin)
 	ASSERT_EQ(expectBill, bill_1.getBill() );
 
 }
+
+TEST(tests, BillForGoldAndSilverAccountWithAdditionalLineWithoutUse)
+{
+	User user_1(SILVER, 1 ,0);
+	BillCounter bill_1(user_1);
+	double expectBill_1 = 29.95;
+
+  User user_2(GOLD, 2 ,0);
+  BillCounter bill_2(user_2);
+  double expectBill_2 = 49.95 + 14.50;
+
+  User user_3(SILVER, 3 ,0);
+  BillCounter bill_3(user_3);
+  double expectBill_3 = 29.95 + 2*21.50;
+
+	 ASSERT_EQ(expectBill_1, bill_1.getBill());
+	 ASSERT_EQ(expectBill_2, bill_2.getBill());
+	 ASSERT_EQ(expectBill_3, bill_3.getBill());
+
+}
