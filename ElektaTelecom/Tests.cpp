8c2911e72ec4aa7cd48555e72457943edfcc9af4
jiaminghuang <4MT-HJM@bitbucket.org>
2016-07-21 05:22:07 -0700

    Add makefile and one simple UT for task-1

---
+++ ElektaTelecom/Tests.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "ElektaTelecom.hpp"
+#include <memory>
+
+TEST(tests, BillForaGoldAccountWithOneLineAndUsesLessThanOneThousandMin)
+{
+	User user_1(GOLD, 1 ,0);
+	BillCounter bill_1(user_1);
+	double expectBill = 49.95;
+	ASSERT_EQ(expectBill, bill_1.getBill() );
+
+}
